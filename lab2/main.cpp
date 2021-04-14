e9ccb816347f38c03e25de8db3d4779cccab24c2
HarmonySeeker <kirilldorovskikh@mail.ru>
2021-04-14 17:41:52 +0700

    rewrote second lab

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -5,24 +5,35 @@
 pthread_cond_t cond;
 pthread_mutex_t mutex;
 
-bool state = true;
+bool state = false;
 
 void *provider(void*) {
-    while(true) {
-        pthread_mutex_lock(&mutex);
-        std::cout << "provider" << std::endl;
-        pthread_cond_signal(&cond);
-        pthread_mutex_unlock(&mutex);
-        sleep(1);
-    }
+   while (true) {
+	pthread_mutex_lock(&mutex);
+	if (state) {
+	     pthread_mutex_unlock(&mutex);
+	}
+	else {
+	     state = !state;
+	     std::cout << "provider" << std::endl;
+	     pthread_cond_signal(&cond);
+	     pthread_mutex_unlock(&mutex);
+	     sleep(1);
+	}
+   } 
 }
 
 void *consumer(void*) {
-    while(true) {
-        pthread_mutex_lock(&mutex);
-        pthread_cond_wait(&cond, &mutex);
-        std::cout << "consumer" << std::endl << std::endl;
-        pthread_mutex_unlock(&mutex);
+    while (true) {
+	    pthread_mutex_lock(&mutex);
+	    while(!state) {
+		pthread_cond_wait(&cond, &mutex);
+	    	std::cout << "wake request" << std::endl;
+	    }
+
+	    state = !state;
+	    std::cout << "consumer" << std::endl << std::endl;
+	    pthread_mutex_unlock(&mutex);
     }
 }
 
@@ -32,11 +43,11 @@ int main() {
     pthread_cond_init(&cond, NULL);
     pthread_mutex_init(&mutex, NULL);
 
-    pthread_create(&thread2, nullptr, consumer, nullptr);
     pthread_create(&thread1, nullptr, provider, nullptr);
+    pthread_create(&thread2, nullptr, consumer, nullptr);
 
-    pthread_join(thread2, nullptr);
     pthread_join(thread1, nullptr);
+    pthread_join(thread2, nullptr);
 
     pthread_cond_destroy(&cond);
     pthread_mutex_destroy(&mutex);
