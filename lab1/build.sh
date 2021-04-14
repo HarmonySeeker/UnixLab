57b74d7a79e8a3c901b63539ba98936e69249a59
HarmonySeeker <kirilldorovskikh@mail.ru>
2021-04-14 14:48:35 +0700

    changed first lab to fulfil requirements

--- lab1/build.sh
+++ lab1/build.sh
@@ -6,31 +6,43 @@ if [ -z $1 ]; then
 else
 	if [ -e $1 ]; then 
 	
-		if [ -z $2 ]; then 
-			echo Error: Invalid output file argument
-			exit 1
-		else
-			TMPFOLDER=`mktemp -d`
-			CURRENTFOLDER=$(pwd)
+		FILENAME=`grep Output $1`
 			
-			trap "rm -Rf $TMPFOLDER; exit 1" SIGINT SIGHUP SIGTERM
+                if [ $? -eq 0 ]; then
 			
-			cp $1 $TMPFOLDER/
-			cd $TMPFOLDER
-			
-			gcc $1 -o $2
-			
-			if [ $? -eq 0 ]; then 
-				mv $2 "$CURRENTFOLDER"
-				cd "$CURRENTFOLDER"
-				rm -Rf $TMPFOLDER
-				echo Success: Build stored at $2
-			else
-				echo Error: Compilation error
-				cd "CURRENTFOLDER"
-				rm -Rf $TMPFOLDER
+			FILENAME=${FILENAME#//Output:}
+
+			echo $FILENAME
+
+			if [ -z $FILENAME ]; then 
+				echo Error: Invalid destination file name
 				exit 1
+			else
+				TMPFOLDER=`mktemp -d`
+				CURRENTFOLDER=$(pwd)
+				
+				trap "rm -Rf $TMPFOLDER; exit 1" SIGINT SIGHUP SIGTERM
+				
+				cp $1 $TMPFOLDER/
+				cd $TMPFOLDER
+				
+				gcc $1 -o $FILENAME
+				
+				if [ $? -eq 0 ]; then 
+					mv $FILENAME "$CURRENTFOLDER"
+					cd "$CURRENTFOLDER"
+					rm -Rf $TMPFOLDER
+					echo Success: Build stored at $FILENAME
+				else
+					echo Error: Compilation error
+					cd "CURRENTFOLDER"
+					rm -Rf $TMPFOLDER
+					exit 1
+				fi
 			fi
+		else
+			echo Error: Destination file name is not specified in the source file.
+			echo Error: Use \"//Output: *file_name*\" in order to specify the destination
 		fi
 	else
 		echo Error: Source file $1 doesn\'t exist
